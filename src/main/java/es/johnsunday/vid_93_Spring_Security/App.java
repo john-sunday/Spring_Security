1ae760b5c989f56582c5300c2d4d51011220949d
tecnicomanotas <domingoortin.j@gmail.com>
2022-06-16 10:41:15 +0200

    Security Spring project creation and change jdk version to 1.8

---
+++ src/main/java/es/johnsunday/vid_93_Spring_Security/App.java
@@ -0,0 +1,13 @@
+package es.johnsunday.vid_93_Spring_Security;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
